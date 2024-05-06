55a4d8267b13e7b9c94aec82b5296adbf7284f14
Abhay Singh <abhay97ps@gmail.com>
2024-05-06 06:23:41 -0400

    docker: containerize django api

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3)%jo_=+95m7#hzvy=gvf^a3d86259zadc4m%nvyt7@-#-gru
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
