4620892b09cf5dc7a9f081cfe0521adcc70e7566
Abhay Singh <abhay97ps@gmail.com>
2024-06-03 20:24:45 -0400

    fix: aws redis host has port init, rm extra

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,9 +61,8 @@ MIDDLEWARE = [
 ]
 
 # Redis Configuration
-REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/1'
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis:6379')
+REDIS_URL = f'redis://{REDIS_HOST}/1'
 
 
 CACHES = {
