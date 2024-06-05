f5e13c328acc0e8eff3ba649b0c1772197d798b9
Abhay Singh <abhay97ps@gmail.com>
2024-06-04 22:35:51 -0400

    fix: rm dotend load

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,9 +1,5 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-# Load environment variables from .env file if it exists
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
