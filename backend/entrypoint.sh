35c8fb5e31dd607fca84090f05d84778983e49c9
Abhay Singh <abhay97ps@gmail.com>
2024-06-08 00:38:30 -0400

    fix: add entrypoint to run migrate inside container

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Daphne
+daphne -b 0.0.0.0 -p 8000 config.asgi:application
