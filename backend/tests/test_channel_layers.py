8d5405e0ae9be938d0bab41d68277282dc203a44
Abhay Singh <abhay97ps@gmail.com>
2024-07-16 13:03:09 -0400

    test improve logging

--- backend/tests/test_channel_layers.py
+++ backend/tests/test_channel_layers.py
@@ -1,12 +1,21 @@
 import channels.layers
 from asgiref.sync import async_to_sync
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def test_channel_layers():
-    channel_layer = channels.layers.get_channel_layer()
-    async_to_sync(channel_layer.send)('test_channel', {'type': 'hello'})
-    print(async_to_sync(
-        channel_layer.receive)('test_channel'))
+    try:
+        logger.info("Getting channel layer...")
+        channel_layer = channels.layers.get_channel_layer()
+        async_to_sync(channel_layer.send)('test_channel', {'type': 'hello'})
+        logger.info("Message sent to test_channel.")
+        response = async_to_sync(channel_layer.receive)('test_channel')
+        logger.info(f"Received message: {response}")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
--- backend/tests/test_redis_conn.py
+++ backend/tests/test_redis_conn.py
@@ -1,5 +1,9 @@
 import os
 import redis
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
 REDIS_PORT = os.getenv('REDIS_PORT', '6379')
@@ -9,9 +13,9 @@ REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}" if ENVIRONMENT == 'local' else 
 
 def test_redis_connection():
     try:
-        print(f"REDIS_HOST: {REDIS_HOST}")
-        print(f"REDIS_PORT: {REDIS_PORT}")
-        print("Creating Redis client...")
+        logger.info(f"REDIS_HOST: {REDIS_HOST}")
+        logger.info(f"REDIS_PORT: {REDIS_PORT}")
+        logger.info("Creating Redis client...")
         r = redis.Redis(
             host=REDIS_HOST,
             port=REDIS_PORT,
@@ -20,15 +24,15 @@ def test_redis_connection():
             ssl=True if ENVIRONMENT == 'production' else False,
             socket_timeout=30
         )
-        print("Redis client created. Pinging...")
+        logger.info("Redis client created. Pinging...")
         if r.ping():
-            print("Connected to Redis successfully!")
+            logger.info("Connected to Redis successfully!")
     except redis.ConnectionError as e:
-        print(f"ConnectionError: Failed to connect to Redis: {e}")
+        logger.error(f"ConnectionError: Failed to connect to Redis: {e}")
     except redis.TimeoutError as e:
-        print(f"TimeoutError: Failed to connect to Redis: {e}")
+        logger.error(f"TimeoutError: Failed to connect to Redis: {e}")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logger.error(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
