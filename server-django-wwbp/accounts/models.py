5e0c6b4289e81fc01c48034df533d2ae10fdb5cc
Abhay Singh <abhay97ps@gmail.com>
2024-08-02 14:21:09 -0400

    alter: restrict deleting an reference object

--- server-django-wwbp/accounts/models.py
+++ server-django-wwbp/accounts/models.py
@@ -16,11 +16,10 @@ class User(AbstractUser):
     interaction_mode = models.CharField(max_length=20, default='text')
 
 
-
 class Module(models.Model):
     name = models.CharField(max_length=100)
     created_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='modules')
+        User, on_delete=models.RESTRICT, related_name='modules')
     content = models.TextField(blank=True, null=True)
     files = models.JSONField(default=list, blank=True, null=True)
 
@@ -29,7 +28,7 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     module = models.ForeignKey(
-        Module, on_delete=models.CASCADE, related_name='tasks')
+        Module, on_delete=models.RESTRICT, related_name='tasks')
     instruction_prompt = models.TextField(blank=True, null=True)
     persona_prompt = models.TextField(blank=True, null=True)
     files = models.JSONField(default=list, blank=True, null=True)
@@ -37,11 +36,11 @@ class Task(models.Model):
 
 class ChatSession(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='chat_sessions')
+        User, on_delete=models.RESTRICT, related_name='chat_sessions')
     module = models.ForeignKey(
-        Module, on_delete=models.CASCADE, related_name='chat_sessions', null=True, blank=True)
+        Module, on_delete=models.RESTRICT, related_name='chat_sessions', null=True, blank=True)
     task = models.ForeignKey(
-        Task, on_delete=models.CASCADE, related_name='chat_sessions', null=True, blank=True)
+        Task, on_delete=models.RESTRICT, related_name='chat_sessions', null=True, blank=True)
     assistant_id = models.TextField(blank=True, null=True)
     thread_id = models.TextField(blank=True, null=True)
     prompt_tokens = models.IntegerField(default=0)
@@ -56,16 +55,10 @@ class ChatSession(models.Model):
             models.Index(fields=['created_at']),
         ]
 
-    class Meta:
-        indexes = [
-            models.Index(fields=['module']),
-            models.Index(fields=['created_at']),
-        ]
-
 
 class ChatMessage(models.Model):
     session = models.ForeignKey(
-        ChatSession, on_delete=models.CASCADE, related_name='messages')
+        ChatSession, on_delete=models.RESTRICT, related_name='messages')
     message = models.TextField()
     sender = models.CharField(max_length=10, choices=(
         ('student', 'Student'), ('bot', 'Bot')))
@@ -79,9 +72,9 @@ class SystemPrompt(models.Model):
 
 
 class UserCSVDownload(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
     module = models.ForeignKey(
-        Module, on_delete=models.CASCADE, related_name='csv_module', null=True, blank=True)
+        Module, on_delete=models.RESTRICT, related_name='csv_module', null=True, blank=True)
     start_date = models.DateField()
     end_date = models.DateField()
     file_url = models.URLField()
