4b40f020263ee67947a9b8289b747bba9ea58b9c
Abhay Singh <abhay97ps@gmail.com>
2025-02-13 15:19:39 -0500

    fix: illegal seek logs

--- server-django-wwbp/entrypoint.sh
+++ server-django-wwbp/entrypoint.sh
@@ -6,5 +6,8 @@ python manage.py migrate
 # Collect static files
 python manage.py collectstatic --noinput
 
+# Tail logs so they appear in STDOUT (optional)
+tail -F /var/log/gunicorn.stdout.log /var/log/gunicorn.stderr.log /var/log/daphne.stdout.log /var/log/daphne.stderr.log &
+
 # Start Supervisor
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
